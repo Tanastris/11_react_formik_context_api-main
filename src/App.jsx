1c18697c53ba786b93c40fb547211179ec1ef461
RokasV <vezikauskas.r@gmail.com>
2024-01-24 09:12:04 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+export default function App() {
+  return (
+    <div className='App container mx-auto'>
+      <h1 className='text-3xl font-bold underline'>Hello, world!</h1>
+      <button className='px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>
+        Nice button
+      </button>
+      <h2>Hello, world!</h2>
+      <p>Hello, world!</p>
+      <hr />
+    </div>
+  );
+}
