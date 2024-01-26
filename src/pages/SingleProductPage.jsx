a8e25d43def46b0255fac3109da78379ae153821
RokasV <vezikauskas.r@gmail.com>
2024-01-26 08:42:39 +0200

    params

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 export default function SingleProductPage() {
+
+  const { prodId } = useParams()
   return (
     <div className='container'>
       <h1 className='text-3xl mb-5'>SingleProductPage</h1>
